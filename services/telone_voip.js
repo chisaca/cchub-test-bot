a4442909f3afe1d282d680b4e4a88a33f14062f7
chisancode <100565382+chisaca@users.noreply.github.com>
2026-02-20 08:34:56 +0200

    Update telone_voip.js

--- services/telone_voip.js
+++ services/telone_voip.js
@@ -1,25 +1,32 @@
 // services/telone_voip.js
+// TelOne VoIP Service (ZiG) - Product ID: 33
+
 const BaseTelOneService = require('./baseTelOneService');
 const { BILLERS } = require('../config/constants');
-
-const config = BILLERS['5']; // TelOne VoIP config
+const hotrecharge = require('./hotrecharge');
 
 class TelOneVoIPService extends BaseTelOneService {
-    constructor(hotrecharge) {
+    constructor() {
+        // Get the complete biller config from constants (key '5' = TelOne VoIP)
+        const billerConfig = BILLERS['5'];
+        
         super(hotrecharge, {
             key: 'telone_voip',
-            name: 'TelOne VoIP',
-            emoji: '📱',
-            currency: 'ZiG',
-            productId: 33,
-            accountTypeId: 1,
-            fee: config.fee,
-            minAmount: config.minAmount,
-            maxAmount: config.maxAmount,
-            requiresAccountNumber: true,
-            requiresNotifyNumber: true
+            name: billerConfig.name,
+            emoji: billerConfig.emoji,
+            currency: billerConfig.currency,
+            productId: billerConfig.productId,
+            accountTypeId: billerConfig.accountTypeId,
+            fee: billerConfig.fee,
+            minAmount: billerConfig.minAmount,
+            maxAmount: billerConfig.maxAmount,
+            requiresAccountNumber: billerConfig.requiresAccountNumber,
+            requiresNotifyNumber: billerConfig.requiresNotifyNumber,
+            accountLength: billerConfig.accountLength,
+            description: billerConfig.description
         });
     }
 }
 
-module.exports = TelOneVoIPService;
+// Export a SINGLETON instance
+module.exports = new TelOneVoIPService();
