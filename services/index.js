83548f039ffc075685db3046ad9547debfad421a
chisancode <100565382+chisaca@users.noreply.github.com>
2026-02-20 08:36:00 +0200

    Update index.js

--- services/index.js
+++ services/index.js
@@ -3,17 +3,33 @@
 const airtimeService = require('./airtime');
 const zesaService = require('./zesa');
 const billsService = require('./bills');
+const nyaradzoService = require('./nyaradzo');
 const emergencyService = require('./emergency');
 const helpService = require('./help');
-const paynowService = require('./paynow'); // Add this
+const paynowService = require('./paynow');
 const hotrecharge = require('./hotrecharge');
+
+// TelOne Services
+const teloneVoice = require('./telone_voice');
+const teloneBroadband = require('./telone_broadband');
+const teloneLte = require('./telone_lte');
+const teloneVoip = require('./telone_voip');
+const teloneUsd = require('./telone_usd');
 
 module.exports = {
     airtimeService,
     zesaService,
     billsService,
+    nyaradzoService,
     emergencyService,
     helpService,
-    paynowService,  // Export it
-    hotrecharge
+    paynowService,
+    hotrecharge,
+    
+    // TelOne Services
+    teloneVoice,
+    teloneBroadband,
+    teloneLte,
+    teloneVoip,
+    teloneUsd
 };
